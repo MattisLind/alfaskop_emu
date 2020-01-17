21e07f9a23327c354af52a445e1eb57c356bd689
Mattis Lind <mattis@mattisborgen.se>
2020-01-17 15:44:19 +0100

    Update SyncFSM.cpp

--- Utils/BSCGateway/BSCGateway/SyncFSM.cpp
+++ Utils/BSCGateway/BSCGateway/SyncFSM.cpp
@@ -15,17 +15,37 @@ The Sync FSM has two states, Hunt state and Sync state. When in Hunt state it wi
 // Construtcor taks as argument a callback to call when sync is established and valid data is received
 
 SyncFSM::syncFSM(void (*recivedSyncData)(unsigned char)) {
-
+  cb = receivedSyncData;
+  dataWord = 0xffffffff;
+  syncState = HUNT; 
 }
 
 // Is called by the data receiver 
 
-SyncFSM::receivedData(unsigned char) {
-
-}
+SyncFSM::receivedData(unsigned char read) {
+  int i;
+  dataWord = dataWord << 8;  
+  dataWord = (0xffffff00 & dataWord) | (0xff & read);
+  if (syncState==HUNT) { // Hunting for SYNC
+    // Try to find sync
+    for (i=0; i<8; i++) {
+      if (((dataWord >> i) & 0xffff) == 0x4c4c) {
+        syncPoint = i;
+        break;
+      }
+    }
+    if (i!= 8) {
+      rxState = SYNC; // We have found sync
+    }
+  }
+  else {
+    data = translationArray[0xff & (dataWord >> syncPoint)];
+    cb (data);
+  } 
+} 
 
 // Routine to tell the Sync FSM to enter Hunt state
 
 SyncFSM::enterHuntState() {
-
+  syncState = HUNT;
 }
