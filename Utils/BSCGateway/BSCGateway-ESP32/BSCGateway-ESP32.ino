6a4e99a1a301209d403c0c53a234c7a6686145d8
Mattis Lind <mattis@mattisbogen.se>
2020-01-20 21:50:02 +0100

    Skeleton for ESP32 part with provisions to run it udner Linux

---
+++ Utils/BSCGateway/BSCGateway-ESP32/BSCGateway-ESP32.ino
@@ -0,0 +1,20 @@
+
+
+// ESP32 main Arduino sketch to read serial data from the BSCGateway-STM32
+// over serial port, process it through the ProtocolFSM and then forward the
+// data over a Ethernet connection to a TN3270 server.
+// SHall include some kind of selection screen to be generated in a 3270
+// data stream so that the terminal can select among many different
+// systems. A nice splash would be great!
+
+
+void setup () {
+
+
+}
+
+
+
+void loop () {
+
+}
