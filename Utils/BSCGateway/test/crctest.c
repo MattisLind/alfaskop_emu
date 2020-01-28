812eeecd2f4ea40c448039851afb896977664afb
Mattis Lind <mattis@mattisbogen.se>
2020-01-28 07:28:23 +0100

    WIP

--- Utils/BSCGateway/test/crctest.c
+++ Utils/BSCGateway/test/crctest.c
@@ -5,9 +5,34 @@
 unsigned short crc16(char *data_p, unsigned short length, unsigned short crc, unsigned short poly);
 
 unsigned char testMessage1 [8] = {0x6c,0xd9,0x02,0x40,0x40,0x40,0x70,0x03};
-unsigned char testMessage2 [8] = {0x6c,0xd9,0x02,0x40,0xc8,0x40,0x50,0x03};
+unsigned char testMessage2 [10] = {0x6c,0xd9,0x02,0x40,0xc8,0x40,0x50,0x03, 0x0d, 0x28};
 unsigned char testMessage1lsb [8];
 unsigned char testMessage2lsb [8];
+
+#include <stdio.h>
+
+int crc163(unsigned char *ptr, int count)
+{
+    unsigned int crc;
+    char i;
+
+    crc = 0x0000;
+    while (--count >= 0)
+    {
+        crc = crc ^ (unsigned int) *ptr++;
+        i = 8;
+        do
+        {
+            if (crc & 0x0001)
+                crc = (crc >> 1) ^ 0xA001;  /* 0x8005 bit reversed */
+            else
+                crc = (crc >> 1);
+        } while(--i);
+    }
+    return (crc);
+}
+
+
 
 
 unsigned crc162(unsigned char *buf, size_t len, unsigned short crc, unsigned short POLY)
@@ -50,7 +75,7 @@ int main () {
   crc = crc16(testMessage1, 8, 0x0000, 0xA001);
   printf ("%04X\n", crc);
   //assert (crc == 0x0000);
-  crc = crc16(testMessage2, 8, 0x0000, 0xA001);
+  crc = crc16(testMessage2, 10, 0x0000, 0xA001);
   printf ("%04X\n", crc);
   //assert (crc == 0x0000);
   crc = crc16(testMessage1lsb, 8, 0x0000, 0xA001);
@@ -62,7 +87,7 @@ int main () {
   crc = crc16(testMessage1, 8, 0xffff, 0xA001);
   printf ("%04X\n", crc);
   //assert (crc == 0x0000);
-  crc = crc16(testMessage2, 8, 0xffff, 0xA001);
+  crc = crc16(testMessage2, 10, 0xffff, 0xA001);
   printf ("%04X\n", crc);
   //assert (crc == 0x0000);
   crc = crc16(testMessage1lsb, 8, 0xffff, 0xA001);
@@ -72,26 +97,18 @@ int main () {
   printf ("%04X\n", crc);
   crc = crc162(testMessage1, 8, 0x0000, 0xA001);
   printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
-  crc = crc162(testMessage2, 8, 0x0000, 0xA001);
+  crc = crc162(testMessage2, 10, 0x0000, 0xA001);
   printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
-  crc = crc162(testMessage1lsb, 8, 0x0000, 0xA001);
-  printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
-  crc = crc162(testMessage2lsb, 8, 0x0000, 0xA001);
-  printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
   crc = crc162(testMessage1, 8, 0xffff, 0xA001);
   printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
-  crc = crc162(testMessage2, 8, 0xffff, 0xA001);
+  crc = crc162(testMessage2, 10, 0xffff, 0xA001);
   printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
-  crc = crc162(testMessage1lsb, 8, 0xffff, 0xA001);
+  crc = crc163(testMessage1, 8);
   printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
-  crc = crc162(testMessage2lsb, 8, 0xffff, 0xA001);
+  crc = crc163(testMessage2, 10);
   printf ("%04X\n", crc);
-  //assert (crc == 0x0000);
+  crc = crc163(testMessage1, 8);
+  printf ("%04X\n", crc);
+  crc = crc163(testMessage2, 10);
+  printf ("%04X\n", crc);
 }
