1c5070f343e64b2963740cd3a74b30d3d7885031
Mattis Lind (matli2) <mattis.lind@teliacompany.com>
2020-01-30 16:02:35 +0100

    WIP

--- Utils/BSCGateway/BSCGateway-STM32/ProtocolFSM.cpp
+++ Utils/BSCGateway/BSCGateway-STM32/ProtocolFSM.cpp
@@ -43,15 +43,34 @@ int PollFSM::sendPoll (unsigned short CU, unsigned short DV) {
     messageFSM.sendENQ(CU, DU);                              // Send the ENQ message
     subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_RTS;           // Goto state wait for RTS
     state = PROTOCOL_FSM_WAIT_FOR_MSG; 
-    mode = PROTOCOL_MODE_POLL;    
+    mode = PROTOCOL_MODE_POLL;
+    thereIsMoreComing=true;    
   }
 }
 
 void PollFSM::sendWrite (unsigned short CU. unsigned short DV, unsigned char * data) {
+  if (state != PROTOCOL_FSM_IDLE) {
+    return -1; // We are busy processing another transaction. Wait!
+  } else {
+    messageFSM.sendENQ(CU, DU);                              // Send the ENQ message
+    subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_RTS;           // Goto state wait for RTS
+    state = PROTOCOL_FSM_WAIT_FOR_MSG; 
+    mode = PROTOCOL_MODE_WRITE;
+    thereIsMoreComing=true;    
+  }
 
 }
 
 void PollFSM::sendRead (unsigned short CU. unsigned short DV, unsigned char * data) {
+  if (state != PROTOCOL_FSM_IDLE) {
+    return -1; // We are busy processing another transaction. Wait!
+  } else {
+    messageFSM.sendENQ(CU, DU);                              // Send the ENQ message
+    subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_RTS;           // Goto state wait for RTS
+    state = PROTOCOL_FSM_WAIT_FOR_MSG; 
+    mode = PROTOCOL_MODE_READ;
+    thereIsMoreComing=true;    
+  }
 
 }
 
@@ -59,14 +78,40 @@ void PollFSM::sendRead (unsigned short CU. unsigned short DV, unsigned char * da
 void ProtcolFSM::receivedMessage( unsigned char type, unsigned char * msg ) {
   switch (mode | state | subState) {
   case PROTOCOL_MODE_POLL | PROTOCOL_FSM_WAIT_FOR_MSG | PROTOCOL_FSM_SUBSTATE_IDLE:
-    switch (type) {
-    case STATUS_MESSAGE:
-      subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_NOT_RTS;
-      state = PROTOCOL_FSM_SENDACK;
-      cnt++;
-      break;
+    if (thereIsMoreComing) {
+      switch (type) {
+        case TEXT_MESSAGE:
+        case TEST_MESSAGE:
+        case STATUS_MESSAGE:
+	  subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_NOT_RTS;
+	  state = PROTOCOL_FSM_SENDACK;
+	  cnt++;
+	  thereIsMoreComing = msg->statusData.thereIsMoreComing;
+	  break;
+        }
+    } else {
+      // Now we should get a EOT if the last message had an ETX
+      if (type == EOT_MESSAGE) {
+	// do the callback. We have aggregated all data into the receive buffer and sends it to initiator
+      } else {
+	// protocol violation
+      }
     }
     break;
+  case PROTOCOL_MODE_WRITE | PROTOCOL_FSM_WAIT_FOR_MSG | PROTOCOL_FSM_SUBSTATE_IDLE:
+    switch (type) {
+    case ACK0_MESSAGE:
+      subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_NOT_RTS;
+      state = PROTOCOL_SEND_FSM_DATA;
+      break;
+    case RVI_MESSAGE:
+    case WACK_MESSAGE:
+      subState = PROTOCOL_FSM_SUBSTATE_WAIT_FOR_NOT_RTS;
+      state = PROTOCOL_FSM_SEND_EOT;
+      // This is some kind of error condition that need to be handled. We store the message and call a callback when we receive the EOT.
+      break;
+
+    }
   default:
 
     // protocol violation
