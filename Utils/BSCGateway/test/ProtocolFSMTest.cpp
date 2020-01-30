e93f565d53a2548b3a55c0d3b203677e1fa09f2d
Mattis Lind <mattis@mattisbogen.se>
2020-01-30 21:13:48 +0100

    Test case poll WIP

--- Utils/BSCGateway/test/ProtocolFSMTest.cpp
+++ Utils/BSCGateway/test/ProtocolFSMTest.cpp
@@ -7,10 +7,37 @@
  */
 #include "../BSCGateway-STM32/ProtocolFSM.h"
 
+void txData (unsigned char);
+void receivedMessage(unsigned char, unsigned char *);
+void enterHuntState();
+void protocolResponse (ProtocolMsg * pMsg);
 
+class MessageFSM messageFSM(txData, receivedMessage, enterHuntState);
+class ProtocolFSM protocolFSM(messageFSM, protocolResponse );
+
+
+void receivedMessage(unsigned char type, unsigned char * msg) {
+  protocolFSM.receivedMessage(type, (MSG *) msg);
+}
+
+void txData (unsigned char ch) {
+}
+void enterHuntState() {
+}
+void protocolResponse (ProtocolMsg * pMsg) {
+}
+
+
+int rts;
 
 
 int main () {
-
-
+  // First send the poll.
+  protocolFSM.sendPoll(0x40,0x40);
+  // Now we need to do a poll to have the worker inside to the work.
+  rts = 0;
+  protocolFSM.workerPoll();
+  rts = 1;
+  protocolFSM.workerPoll();
+  
 }
