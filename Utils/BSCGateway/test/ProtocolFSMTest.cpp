9c00d5963efd0439baad893857c3b9e48735373d
Mattis Lind (matli2) <mattis.lind@teliacompany.com>
2020-01-31 09:18:15 +0100

    WIP

--- Utils/BSCGateway/test/ProtocolFSMTest.cpp
+++ Utils/BSCGateway/test/ProtocolFSMTest.cpp
@@ -6,14 +6,45 @@
 
  */
 #include "../BSCGateway-STM32/ProtocolFSM.h"
+#include <assert.h>
+#include <stdio.h>
+
 
 void txData (unsigned char);
 void receivedMessage(unsigned char, unsigned char *);
 void enterHuntState();
 void protocolResponse (ProtocolMsg * pMsg);
 
+int ctsValue;
+
+void digitalWrite(int pin, int value) {
+  ctsValue  = value;
+}
+
+
+int rtsValue;
+
+int digitalRead (int pin) {
+  return rtsValue;
+}
+
+
+#define RTS 1  
+#define CTS 2
+
+bool rtsIsSet() {
+  return digitalRead(RTS)==1;
+}
+
+void cts(bool value) {
+  digitalWrite(CTS, value?1:0);
+}
+
+
+
+
 class MessageFSM messageFSM(txData, receivedMessage, enterHuntState);
-class ProtocolFSM protocolFSM(messageFSM, protocolResponse );
+class ProtocolFSM protocolFSM(messageFSM, protocolResponse, rtsIsSet, cts );
 
 
 void receivedMessage(unsigned char type, unsigned char * msg) {
@@ -28,16 +59,14 @@ void protocolResponse (ProtocolMsg * pMsg) {
 }
 
 
-int rts;
-
 
 int main () {
   // First send the poll.
   protocolFSM.sendPoll(0x40,0x40);
   // Now we need to do a poll to have the worker inside to the work.
-  rts = 0;
+  rtsValue = 0;
   protocolFSM.workerPoll();
-  rts = 1;
+  rtsValue = 1;
   protocolFSM.workerPoll();
-  
+  assert (ctsValue == 1);
 }
