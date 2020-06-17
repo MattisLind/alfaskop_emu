c243fa95115224cdb2cc61fdb2cfe123b75bf4f7
Mattis Lind <mattis@mattisborgen.se>
2020-06-17 22:19:53 +0200

    fixed interconnect between messageFMSs.

--- Utils/BSCGateway/BSCBridge/BSCBridge.ino
+++ Utils/BSCGateway/BSCBridge/BSCBridge.ino
@@ -1,6 +1,13 @@
+
+/*
+ * Set board to Generic STM32F103C form STM32duino.com Variant STM32F103C8, Upload method STLink, 72 MHz
+ * 
+ */
+
+
 #include <SPI.h>
 #include "ebcdic.h"
-#include "lsbmsb.h"
+#include "lsbmsb.h" 
 #include "RingBuffer.h"
 #include "SyncFSM.h"
 #include "MessageFSM.h"
@@ -11,6 +18,17 @@
 
 SPIClass SPI_2(2); 
 
+
+void txDataCallback (unsigned char);
+void enterHuntStateCallback ();
+void messageReceivedCallback (unsigned char, unsigned char *);
+void txToHercules (unsigned char);
+void messageReceivedFromHerculesCallback (unsigned char, unsigned char *);
+
+class MessageFSM messageFSM(txDataCallback, messageReceivedCallback, enterHuntStateCallback);
+class MessageFSM herculesMessageFSM(txToHercules, messageReceivedFromHerculesCallback, NULL, true);
+
+
 HardwareTimer pwmtimer(1);
 const int pwmOutPin = PA8; // pin10
 
@@ -20,7 +38,7 @@ void setup() {
   int maxduty;
   // put your setup code here, to run once:
   pwmtimer.pause();
-  period =104; // PWM period in useconds, freq 300 Hz
+  period =13; // PWM period in useconds, freq 300 Hz
   maxduty = pwmtimer.setPeriod(period);
   pinMode(pwmOutPin, PWM);
   pwmtimer.refresh();
@@ -61,11 +79,116 @@ void receiveCallback(unsigned char ch) {
 
 
 void messageReceivedCallback(unsigned char msgType, unsigned char * msg) {
-     // Serialize and send messgae over serial port
+  // Dispatch message coming from terminal based on msgType and let the hercules facing take on.
+  switch (msgType) {
+    case EOT_MESSAGE:
+      Serial.println("Received EOT from Cluster controller");
+      herculesMessageFSM.sendEOT();
+      break;
+    case ENQ_MESSAGE:
+      Serial.print("Received ENQ for CU=");
+      Serial.print(((MSG *) msg)->enqData.CU, HEX);
+      Serial.print(" DV=");
+      Serial.print(((MSG *) (msg))->enqData.DV, HEX);
+      Serial.println( "from Cluster controller");
+      herculesMessageFSM.sendENQ(((MSG *) msg)->enqData.CU, ((MSG *) (msg))->enqData.DV ); 
+      break;
+    case NAK_MESSAGE:
+      Serial.print("Received NAK from Cluster controller");
+      herculesMessageFSM.sendNAK();
+      break;
+    case ACK0_MESSAGE:
+      Serial.print("Received ACK0 from Cluster controller");
+      herculesMessageFSM.sendACK0();
+      break;
+    case ACK1_MESSAGE:
+      Serial.print("Received ACK1 from Cluster controller");
+      herculesMessageFSM.sendACK1();
+      break;
+    case WACK_MESSAGE:
+      Serial.print("Received WACK from Cluster controller");
+      herculesMessageFSM.sendWACK();
+      break;
+    case RVI_MESSAGE:
+      Serial.print("Received RVI from Cluster controller");
+      herculesMessageFSM.sendRVI();
+      break;               
+    case STATUS_MESSAGE:
+      Serial.print("Received STATUS for CU=");
+      Serial.print(((MSG *) msg)->statusData.CU, HEX);
+      Serial.print(" DV=");
+      Serial.print(((MSG *) (msg))->statusData.DV, HEX);
+      Serial.println( "from Cluster controller");      
+      herculesMessageFSM.sendStatusMessage(((MSG *) msg)->statusData.CU, ((MSG *) msg)->statusData.DV, ((MSG *) msg)->statusData.status, ((MSG *) msg)->statusData.sense);
+      break;               
+    case TEXT_MESSAGE:
+      Serial.print("Received TEXT from Cluster controller");
+      herculesMessageFSM.sendTextMessage(((MSG *) msg)->textData.length, ((MSG *) msg)->textData.msg, ((MSG *) msg)->textData.thereIsMoreComing);
+      break;               
+    case TEST_MESSAGE:
+      Serial.print("Received TEST from Cluster controller");
+      herculesMessageFSM.sendTestRequestMessage(((MSG *) msg)->testData.length, ((MSG *) msg)->testData.msg, ((MSG *) msg)->testData.thereIsMoreComing);
+      break;               
+    case ERROR_MESSAGE:
+      break;               
+                 
+  }
 }
 
 void messageReceivedFromHerculesCallback(unsigned char msgType, unsigned char * msg) {
-     // Serialize and send messgae over serial port
+  // Dispatch message coming from terminal based on msgType and let the hercules facing take on.
+  switch (msgType) {
+    case EOT_MESSAGE:
+      Serial.println("Received EOT from Hercules"); 
+      messageFSM.sendEOT();
+      break;
+    case ENQ_MESSAGE:
+      Serial.print("Received ENQ for CU=");
+      Serial.print(((MSG *) msg)->enqData.CU, HEX);
+      Serial.print(" DV=");
+      Serial.print(((MSG *) (msg))->enqData.DV, HEX);
+      Serial.println( "from Hercules");    
+      messageFSM.sendENQ(((MSG *) msg)->enqData.CU, ((MSG *) (msg))->enqData.DV ); 
+      break;
+    case NAK_MESSAGE:
+      Serial.print("Received NAK from Hercules");    
+      messageFSM.sendNAK();
+      break;
+    case ACK0_MESSAGE:
+      Serial.print("Received ACK0 from Hercules");
+      messageFSM.sendACK0();
+      break;
+    case ACK1_MESSAGE:
+      Serial.print("Received ACK1 from Hercules");
+      messageFSM.sendACK1();
+      break;
+    case WACK_MESSAGE:
+      Serial.print("Received WACK from Hercules");
+      messageFSM.sendWACK();
+      break;
+    case RVI_MESSAGE:
+      Serial.print("Received RVI from Hercules");
+      messageFSM.sendRVI();
+      break;               
+    case STATUS_MESSAGE:
+      Serial.print("Received STATUS for CU=");
+      Serial.print(((MSG *) msg)->statusData.CU, HEX);
+      Serial.print(" DV=");
+      Serial.print(((MSG *) (msg))->statusData.DV, HEX);
+      Serial.println( "from Hercules");
+      messageFSM.sendStatusMessage(((MSG *) msg)->statusData.CU, ((MSG *) msg)->statusData.DV, ((MSG *) msg)->statusData.status, ((MSG *) msg)->statusData.sense);
+      break;               
+    case TEXT_MESSAGE:
+      Serial.print("Received TEXT from Hercules");
+      messageFSM.sendTextMessage(((MSG *) msg)->textData.length, ((MSG *) msg)->textData.msg, ((MSG *) msg)->textData.thereIsMoreComing);
+      break;               
+    case TEST_MESSAGE:
+      Serial.print("Received TEST from Hercules");
+      messageFSM.sendTestRequestMessage(((MSG *) msg)->testData.length, ((MSG *) msg)->testData.msg, ((MSG *) msg)->testData.thereIsMoreComing);
+      break;               
+    case ERROR_MESSAGE:
+      break;                                
+  }
 }
 
 void txDataCallback (unsigned char ch) {
@@ -83,8 +206,7 @@ void enterHuntStateCallback () {
      syncFSM.enterHuntState();     
 }
 
-class MessageFSM messageFSM(txDataCallback, messageReceivedCallback, enterHuntStateCallback);
-class MessageFSM herculesMessageFSM(txToHercules, messageReceivedFromHerculesCallback, NULL, true);
+
 
 void loop() {
 char ch;
